46e7970c340e516cbcfaf7dc3816e66bf949fba9
varies87 <27harth@gmail.com>
2026-06-30 13:06:06 -0500

    Update create-payment-intent.js

--- api/create-payment-intent.js
+++ api/create-payment-intent.js
@@ -125,7 +125,7 @@ async function handleCreatePaymentIntent(body) {
 module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "OPTIONS") {
     res.status(200).end();
