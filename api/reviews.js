b5bbacc37aebbe62aeb3cc63ca31516fb2d55546
varies87 <27harth@gmail.com>
2026-06-30 13:05:36 -0500

    Update reviews.js

--- api/reviews.js
+++ api/reviews.js
@@ -235,7 +235,7 @@ async function handleReviewsRequest(body) {
 module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "OPTIONS") {
     res.status(200).end();
