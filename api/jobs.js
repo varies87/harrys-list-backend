cbfbb799b00ad7be60e2c3d994f81ec85a22ed0f
varies87 <27harth@gmail.com>
2026-06-30 13:02:47 -0500

    Update jobs.js

--- api/jobs.js
+++ api/jobs.js
@@ -424,7 +424,7 @@ async function handleJobsRequest(body) {
 module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "OPTIONS") {
     res.status(200).end();
